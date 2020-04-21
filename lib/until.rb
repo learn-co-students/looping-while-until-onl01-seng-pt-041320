ce12ffdc635b2f99226a9aad282456aa3f61e465
Lamont30265 <L.harris008@icloud.com>
2020-04-20 20:00:55 -0400

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,11 @@
 def using_until
   levitation_force = 6
-  #your code here
-    
+  
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end
+
 end
 
+# One helpful way to think about it is to read UNTIL as "if not".
